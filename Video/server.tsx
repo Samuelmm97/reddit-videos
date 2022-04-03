c9ff908eba5a30f85b1f7178ecadfada4352755c
Samuelmm97 <sammymenaged@yahoo.com>
2022-04-03 12:08:56 -0400

    automatic uploads

--- Video/server.tsx
+++ Video/server.tsx
@@ -15,35 +15,49 @@ import express from 'express';
 import fs from 'fs';
 import os from 'os';
 import path from 'path';
-import {getAudioDuration} from '@remotion/media-utils';
 import {textToSpeech} from './src/TextToSpeech';
 import fetch from 'cross-fetch';
 import md5 from 'md5';
+import FormData from 'form-data';
+import bodyParser from 'body-parser';
+import {google} from 'googleapis';
+const {OAuth2} = google.auth;
 globalThis.fetch = fetch;
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true, limit: '1000mb'}));
 const port = process.env.PORT || 8000;
 const compositionId = 'HelloWorld';
 
 const cache = new Map<string, string>();
 
-app.get('/', async (req, res) => {
-	const sendFile = async(file: string) => {
-		
-		var test = fs.createReadStream(file)
-			.pipe(res)
-			.on('close', () => {
-				res.end();
-			});
-		console.log(test);
-		let options:RequestInit = {method:"POST", body:JSON.stringify({readStream: test})};
-		const uploadResponse = await fetch(`http://${process.env.IP}:5000/upload`, options);
-	};
+setTimeout(
+	() =>
+		create(
+			{query: null},
+			{
+				end: () => console.log('uploading...'),
+				set: (str, str2) => console.log(''),
+				json: () => console.log(''),
+			}
+		),
+	3000
+);
+
+async function create(req, res) {
+	// const sendFile = async (file: string) => {
+	// 	fs.createReadStream(file)
+	// 		.pipe(res)
+	// 		.on('close', () => {
+	// 			res.end();
+	// 		});
+	// };
 	try {
-		if (cache.get(JSON.stringify(req.query))) {
-			sendFile(cache.get(JSON.stringify(req.query)) as string);
-			return;
-		}
+		// if (cache.get(JSON.stringify(req.query))) {
+		// 	sendFile(cache.get(JSON.stringify(req.query)) as string);
+		// 	return;
+		// }
 
 		const response = await fetch(`http://${process.env.IP}:3100/top-posts`);
 		const json: any = await response.json();
@@ -162,13 +176,20 @@ app.get('/', async (req, res) => {
 			assetsInfo,
 		});
 		cache.set(JSON.stringify(req.query), finalOutput);
-		sendFile(finalOutput);
-		
-		
+		// sendFile(finalOutput);
+		readStreamInput = finalOutput;
+		authorize();
 
-
-
-
+		// const formData = new FormData({maxDataSize: Infinity});
+		// formData.append('file', readStream as any);
+		// const options: RequestInit = {
+		// 	method: 'POST',
+		// 	body: formData as any,
+		// };
+		// const uploadResponse = await fetch(
+		// 	`http://${process.env.IP}:5000/upload`,
+		// 	options
+		// );
 
 		console.log('Video rendered and sent!');
 	} catch (err) {
@@ -177,7 +198,137 @@ app.get('/', async (req, res) => {
 			error: err,
 		});
 	}
+}
+
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/youtube-nodejs-quickstart.json
+const SCOPES = ['https://www.googleapis.com/auth/youtube.upload'];
+const TOKEN_DIR =
+	(process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) +
+	'/.credentials/';
+const TOKEN_PATH = './token.json';
+let readStreamInput = null;
+
+// Authorize a client with the loaded credentials, then call the YouTube API.
+const clientSecret = process.env.CLIENT_SECRET;
+const clientId = process.env.CLIENT_ID;
+const redirectUrl = 'http://localhost:8000/oauth2callback';
+const oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+
+function authorize() {
+	// Check if we have previously stored a token.
+	fs.readFile(TOKEN_PATH, (err, token) => {
+		if (err || JSON.parse(token.toString()).expiry_date < Date.now()) {
+			getNewToken(oauth2Client);
+		} else {
+			oauth2Client.credentials = JSON.parse(token.toString());
+			uploadVideo(oauth2Client, readStreamInput);
+		}
+	});
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client) {
+	const authUrl = oauth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope: SCOPES,
+	});
+	console.log('Authorize this app by visiting this url: ', authUrl);
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+	try {
+		fs.mkdirSync(TOKEN_DIR);
+	} catch (err) {
+		if (err.code != 'EEXIST') {
+			throw err;
+		}
+	}
+	fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+		if (err) throw err;
+		console.log('Token stored to ' + TOKEN_PATH);
+	});
+}
+
+/**
+ * Lists the names and IDs of up to 10 files.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ * @param {string} readStream The URL for the video upload
+ */
+function uploadVideo(auth, readStream) {
+	const service = google.youtube('v3');
+	service.videos.insert(
+		{
+			auth,
+			// resource: {
+			// 	// Video title and description
+			// 	snippet: {
+			// 		title: 'Testing YoutTube API NodeJS module',
+			// 		description: 'Test video upload via YouTube API',
+			// 	},
+			// 	status: {
+			// 		privacyStatus: 'public',
+			// 	},
+			// },
+			requestBody: {
+				snippet: {
+					title: 'Testing YoutTube API NodeJS module',
+					description: 'Test video upload via YouTube API',
+				},
+				status: {
+					privacyStatus: 'public',
+				},
+			},
+			// This is for the callback function
+			part: ['snippet', 'status'],
+
+			// Create the readable stream to upload the video
+			media: {
+				body: fs.createReadStream(readStream),
+			},
+		},
+		(err, data) => {
+			console.log('Done.', err, data);
+			process.exit();
+		}
+	);
+}
+
+app.get('/oauth2callback', (req, res) => {
+	const {code} = req.query;
+	oauth2Client.getToken(code as string, (err, token) => {
+		if (err) {
+			console.log('Error while trying to retrieve access token', err);
+			return;
+		}
+		oauth2Client.credentials = token;
+		storeToken(token);
+		uploadVideo(oauth2Client, readStreamInput);
+	});
+	console.log(req);
 });
+
+app.get('/', create);
 
 app.listen(port);
 
